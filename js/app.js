bcfd18f0693723f0d273d6c792b66adc4f0776f9
simplonfl <fabrice-laroche81@live.fr>
2018-01-04 09:39:45 +0100

    Base JS & HTML Décompte UTC Exemple

--- js/app.js
+++ js/app.js
@@ -1,8 +1,16 @@
-function main(){
+JQuery(function($){
 
-  function Kountdown(){
-    var h = 
+  var launch = new Date(2014,03,10,10,0,0);          
+  setDate();
+
+    function setDate(){
+      var now = Date();
+      var s = (launch.getTime() - now.getTime())/1000;
+      var d = Math.floor(s/86400);
+      $('#days').html('<strong>'+d+'</strong>');
   }
+
+});
 
 
 
