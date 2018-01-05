f042c4bec7f9af6b3b2457cad67bc4b3c8a3a07f
simplonfl <fabrice-laroche81@live.fr>
2018-01-05 12:17:28 +0100

    modification total/repart de zéro

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 JQuery(function($){
 
-  var launch = new Date(2014,03,10,10,0,0);          
+  var launch = new Date(2014,03,10,10,0,0);
   setDate();
 
     function setDate(){
@@ -8,7 +8,9 @@ JQuery(function($){
       var s = (launch.getTime() - now.getTime())/1000;
       var d = Math.floor(s/86400);
       $('#days').html('<strong>'+d+'</strong>');
-  }
+    }
+
+})
 
 });
 
